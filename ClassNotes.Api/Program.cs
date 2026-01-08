54f36248e324c250953cf62e82b4ecd67145aa5e
Eric König <120642443+Sonrotz0815@users.noreply.github.com>
2026-01-08 11:36:16 +0100

    crafted frontend

--- ClassNotes.Api/Program.cs
+++ ClassNotes.Api/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("http://localhost:5275", "https://localhost:7213")
+        policy.WithOrigins("http://localhost:5275","http://localhost:5173","https://localhost:7213")
         //policy.WithOrigins("https://diwiatschool.github.io")
               .AllowAnyHeader()
               .AllowAnyMethod();
