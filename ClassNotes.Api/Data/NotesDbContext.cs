cf3a1963fc5c8c9e1f63d71e99e2f51bde3bbb1a
Eric König <120642443+Sonrotz0815@users.noreply.github.com>
2026-01-08 10:21:13 +0100

    Added API Structure and DB

---
+++ ClassNotes.Api/Data/NotesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ClassNotes.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassNotes.Api.Data
+{
+    public class NotesDbContext : DbContext
+    {
+        
+        public NotesDbContext(DbContextOptions<NotesDbContext> options) : base(options) { }
+        
+        public DbSet<Note> Notes { get; set; }
+    
+    }
+}
